c247b435f8f85333e61049a6efc35543f6d73f2a
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-31 03:45:48 +0700

    Menambahkan fitur remove pada card item di keranjang

--- src/components/CardCart.jsx
+++ src/components/CardCart.jsx
@@ -1,7 +1,15 @@
+import ButtonSmallCircle from "./ButtonSmallCircle"
 import QtyComponent from "./QtyComponent"
+import iconDelete from '../assets/img/delete.png'
+import { useDispatch } from "react-redux"
+import { removeItemById } from "../redux/slices/cartSlice"
 
 export default function CardCart({ title, price, image, qty, idItem }){
     
+    const dispatch = useDispatch()
+
+    const handleDeleteItem = () => dispatch(removeItemById(idItem))
+
     return (
         <div className="flex flex-col items-left gap-4 mt-3">
             <div className="flex items-center gap-3">
@@ -10,7 +18,13 @@ export default function CardCart({ title, price, image, qty, idItem }){
             </div>
             <div className="flex items-center justify-between gap-5">
                 <div className="font-semibold">${ price }</div>
-                <QtyComponent qty={ qty } idItem={ idItem } />
+
+                <div className="flex gap-5">
+                    <QtyComponent qty={ qty } idItem={ idItem } />
+                    <div className="bg-red-500 rounded-full p-2 cursor-pointer" onClick={ handleDeleteItem }>
+                        <img src={iconDelete} className="w-[20px] h-[20px]" />
+                    </div>
+                </div>
             </div>
         </div>
     )
