a8355b1c2e433513c674a71c435ed4331f8dafd7
Andy Eka Mulya <andymulya13@gmail.com>
2023-09-03 04:09:18 +0700

    Menambahkan feature ketika klik category, di body beranda akan menampilkan product dengan sesuai categori yang dipilih

--- src/components/beranda/CategoryList.jsx
+++ src/components/beranda/CategoryList.jsx
@@ -4,12 +4,19 @@ import iconElectronic from "../../assets/img/electronic.png"
 import iconJewelery from "../../assets/img/jewelery.png"
 import iconMenTshirt from "../../assets/img/men_tshirt.png"
 import iconWomenTshirt from "../../assets/img/women_tshirt.png"
+import { useDispatch } from "react-redux"
+import { getInputSearch } from "../../redux/slices/searchSlice"
 
 export default function CategoryList(){
     const { data, isSuccess } = useGetAllCategoriesQuery()
+    const dispatch = useDispatch()
     const imgCategory = [iconElectronic, iconJewelery, iconMenTshirt, iconWomenTshirt]
+
+    const handleGetProductByCategory = (category) => {
+        dispatch(getInputSearch(category.toLowerCase()))
+    }
     
     return isSuccess && data.map((category, i) => {
-        return <CategoryComponent key={ i } titleCategory={ category } image={ imgCategory[i] } />
+        return <CategoryComponent key={ i } titleCategory={ category } image={ imgCategory[i] } handleOnClick={() => handleGetProductByCategory(category)} />
     }) 
 }
